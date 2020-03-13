a01c2c9f65bf2c2503a4c6351efcdeb3a64304e4
Axel Froborg <axel.froborg@cdon.com>
2020-03-13 19:42:09 +0100

    moved randomidgenerator so it doesnt generate the same ids

--- functions/src/services/mongodb-service.ts
+++ functions/src/services/mongodb-service.ts
@@ -2,38 +2,46 @@ import { Document } from 'mongoose';
 import { IShrunkenLink } from '../models/IShrunkenLink';
 import { ShrunkenLink } from '../models/ShrunkenLink';
 
+const randomId = (): string => {
+  return Math.random()
+    .toString(36)
+    .substr(2, 9);
+};
+
 export const newLink = (link: string): Promise<Document> => {
-    return new Promise((resolve, reject) => {
-        const shrunkenLink = new ShrunkenLink({ originalLink: link });
-        shrunkenLink.save()
-            .then((data) => resolve(data))
-            .catch(err => {
-                console.error(err);
-                reject(err);
-            })
-    })
-}
+  return new Promise((resolve, reject) => {
+    const shrunkenLink = new ShrunkenLink({
+      originalLink: link,
+      shortLink: randomId()
+    });
+    shrunkenLink
+      .save()
+      .then(data => resolve(data))
+      .catch(err => {
+        console.error(err);
+        reject(err);
+      });
+  });
+};
 
 export const getLink = (shortLink: string): Promise<string> => {
-    return new Promise((resolve, reject) => {
-        ShrunkenLink.findOne({ shortLink: shortLink.trim() })
-            .then((link: IShrunkenLink | null) => {
-                if (link) {
-                    link.clicks!++;
-                    link.save()
-                        .then(() => resolve(link.originalLink))
-                        .catch((err: any) => {
-                            console.error(err);
-                            reject(err);
-                        })
-
-                }
-                else
-                    reject('Link does not exist');
-            })
+  return new Promise((resolve, reject) => {
+    ShrunkenLink.findOne({ shortLink: shortLink.trim() })
+      .then((link: IShrunkenLink | null) => {
+        if (link) {
+          link.clicks!++;
+          link
+            .save()
+            .then(() => resolve(link.originalLink))
             .catch((err: any) => {
-                console.error(err);
-                reject(err);
-            })
-    });
-}
+              console.error(err);
+              reject(err);
+            });
+        } else reject('Link does not exist');
+      })
+      .catch((err: any) => {
+        console.error(err);
+        reject(err);
+      });
+  });
+};
