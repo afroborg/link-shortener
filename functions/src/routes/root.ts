84d587aed641a03a6d21320d8b32f11672e8c6fe
Axel Froborg <axel.froborg@cdon.com>
2020-03-13 19:26:28 +0100

    changed post to /new as it otherwise was directed to vuepage

--- functions/src/routes/root.ts
+++ functions/src/routes/root.ts
@@ -3,17 +3,16 @@ import { getLink, newLink } from '../services/mongodb-service';
 const app = Router();
 
 app.get('/:shortLink', (req: Request, res: Response): void => {
-    const shortLink = req.params.shortLink;
-    getLink(shortLink)
-        .then((originalString: string) => res.redirect(originalString))
-        .catch((err: any) => res.status(404).send(err))
-
+  const shortLink = req.params.shortLink;
+  getLink(shortLink)
+    .then((originalString: string) => res.redirect(originalString))
+    .catch((_err: any) => res.redirect('/'));
 });
 
-app.post('/', (req: Request, res: Response) => {
-    newLink(req.body.link)
-        .then((data) => res.send(data))
-        .catch(err => res.status(500).send(err))
+app.post('/new', (req: Request, res: Response) => {
+  newLink(req.body.link)
+    .then(data => res.send(data))
+    .catch(err => res.status(500).send(err));
 });
 
 export default app;
